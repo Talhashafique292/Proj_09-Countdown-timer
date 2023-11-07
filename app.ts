07e8c023897d9020b38febe2fd174029e5340c23
Talhashafique292 <talhashafique292@gmail.com>
2023-11-07 23:34:06 +0500

    Initial commit

---
+++ app.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+import { differenceInSeconds } from "date-fns";
+import inquirer from "inquirer";
+
+
+const res = await inquirer.prompt({
+    type: "number",
+    name: "UserInput",
+    message: "Please enter the ammount of second",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please enter valid number"
+        } else if (input > 60) {
+            return "seconds must be in range of 0-60"
+        } else {
+            return true; 
+        }
+    }
+})
+
+let input = res.UserInput
+function startTime(val: number) {
+    const initialTime = new Date().setSeconds(new Date().getSeconds() + val)
+    const intervalTime = new Date(initialTime);
+    setInterval((() => {
+        const currentTime = new Date();
+        const timeDifference = differenceInSeconds(intervalTime, currentTime);
+        if (timeDifference <= 0) {
+            console.log('Timer has expired')
+            process.exit()
+        }
+        const min = Math.floor((timeDifference % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDifference % 60);
+        console.log(`${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`);
+    }), 1000);
+}
+
+startTime(input)
+
+
